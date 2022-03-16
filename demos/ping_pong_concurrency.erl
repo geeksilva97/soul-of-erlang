c97e1692d3e53f9e7cf34177c51247544259f644
geeksilva97 <edigleyssonsilva@gmail.com>
2022-03-15 23:53:17 -0300

    add concurrency ping pong example

---
+++ demos/ping_pong_concurrency.erl
@@ -0,0 +1,30 @@
+-module(ping_pong_concurrency).
+
+-export([start/0, ping/2, pong/0]).
+
+ping(0, Pong_PID) ->
+    Pong_PID ! finished,
+    io:format("ping finished~n", []);
+
+ping(N, Pong_PID) ->
+    timer:sleep(1000),
+    Pong_PID ! {ping, self()},
+    receive
+        pong ->
+            io:format("Ping received pong~n", [])
+    end,
+    ping(N - 1, Pong_PID).
+
+pong() ->
+    receive
+        finished ->
+            io:format("Pong finished~n", []);
+        {ping, Ping_PID} ->
+            io:format("Pong received ping~n", []),
+            Ping_PID ! pong,
+            pong()
+    end.
+
+start() ->
+    Pong_PID = spawn(ping_pong_concurrency, pong, []),
+    spawn(ping_pong_concurrency, ping, [3, Pong_PID]).
