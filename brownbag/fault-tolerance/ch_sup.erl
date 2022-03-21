5e9b4498f59b55a2203efb80e4c7a90669b7a99e
geeksilva97 <edigleyssonsilva@gmail.com>
2022-03-20 23:02:24 -0300

    fix supervisor

--- brownbag/fault-tolerance/ch_sup.erl
+++ brownbag/fault-tolerance/ch_sup.erl
@@ -5,9 +5,9 @@
 -export([init/1]).
 
 start_link() ->
-  supervisor:start_link(ch_sup, []).
+    supervisor:start_link(ch_sup, []).
 
 init(_Args) ->
-  {ok, {one_for_one, 1, 60},
-        [{ch3, {ch3, start_link, []},
-         permanent, brutal_kill, worker, [ch3]}]}.
+    {ok, {{one_for_one, 1, 60},
+          [{ch3, {ch3, start_link, []},
+            permanent, brutal_kill, worker, [ch3]}]}}.
