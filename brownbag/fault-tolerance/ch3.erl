65070501fcc85e829b566a236cafdf3c4c480a59
geeksilva97 <edigleyssonsilva@gmail.com>
2022-03-20 23:01:35 -0300

    add brownbag folder

---
+++ brownbag/fault-tolerance/ch3.erl
@@ -0,0 +1,36 @@
+-module(ch3).
+-behaviour(gen_server).
+
+% Functions needed for gen_server
+-export([init/1, handle_call/3, handle_cast/2]).
+-export([decrement/0, increment/0, get_state/0, stop/0, terminate/2, start_link/0]).
+
+start_link() ->
+  gen_server:start_link({local,ch3}, ch3, [], []).
+
+init(_Args) ->
+  {ok, 0}.
+
+increment() ->
+  gen_server:cast(ch3, increment).
+
+decrement() ->
+  gen_server:cast(ch3, decrement).
+
+get_state() ->
+  gen_server:call(ch3, get).
+
+stop() ->
+  gen_server:cast(ch3, stop).
+
+handle_cast(stop, State) ->
+  {stop, normal, State};
+handle_cast(increment, State) ->
+  {noreply, State + 1};
+handle_cast(decrement, State) ->
+  {noreply, State - 1}.
+
+handle_call(get, _From, State) ->
+  {reply, State, State}.
+
+terminate(normal, _State) -> ok.
---
+++ brownbag/fault-tolerance/ch_sup.erl
@@ -0,0 +1,13 @@
+-module(ch_sup).
+-behaviour(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link(ch_sup, []).
+
+init(_Args) ->
+  {ok, {one_for_one, 1, 60},
+        [{ch3, {ch3, start_link, []},
+         permanent, brutal_kill, worker, [ch3]}]}.
